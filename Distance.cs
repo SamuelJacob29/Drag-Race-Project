540e08eb7debc680a9c69c6da4d06922db64a52c
Samuel Jacob Bostic <sbostic@unca.edu>
2018-10-29 14:52:29 -0400

    Add files via upload

---
+++ Distance.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distance : MonoBehaviour {
+    public GameObject Car;
+    public GameObject FinishLine;
+
+    public float distanceRightCar;
+    public float distanceLeftCar;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        distanceRightCar = Vector3.Distance(Car.transform.position, FinishLine.transform.position);
+        distanceLeftCar = Vector3.Distance(Car.transform.position, FinishLine.transform.position);
+
+        if (distanceRightCar < 12)
+        {
+            Debug.Log("The Right Car won.");
+        }else if(distanceLeftCar < 12)
+        {
+            Debug.Log("The Left Car won.");
+        }
+    
+	}
+}
